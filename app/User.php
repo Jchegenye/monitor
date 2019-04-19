00cccf6c1917bdd9be94a0be7e173d0a39b9f221
Jackson Chegenye <chegenyejackson@gmail.com>
2019-04-19 19:38:10 +0300

    General app setup

---
+++ app/User.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Monitor;
+
+use Illuminate\Notifications\Notifiable; //Send emails
+use Jenssegers\Mongodb\Eloquent\Model as Eloquent;
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Auth\Authenticatable as AuthenticableTrait;
+use Illuminate\Auth\Passwords\CanResetPassword;
+use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
+
+class User extends Eloquent  implements Authenticatable,CanResetPasswordContract
+{
+    use Notifiable,AuthenticableTrait,CanResetPassword;
+
+    protected $dates = ['deleted_at'];
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+}
